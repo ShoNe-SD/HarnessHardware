4d910da81707b616a0def98ec93a2f3352c5ae72
Nebojsa Vujicic <shonesd93@gmail.com>
2023-10-18 02:32:04 +0200

    settings fix

--- HarnessHardware/settingsprod.py
+++ HarnessHardware/settingsprod.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'HarnessHardware.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "harnesshardware",
+        'USER': "Kristijan",
+        'PASSWORD': "Krompirpire#123",
+        'HOST': "localhost",
+        'PORT': "",
     }
 }
 
